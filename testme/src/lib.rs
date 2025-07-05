a85f6c038c1264e1ee5fb666d587f7de1ec2b612
nikita-skobov <nskobov.websites@gmail.com>
2025-07-05 13:47:38 -0600

    fix signature, add submit_tests fn

--- testme/src/lib.rs
+++ testme/src/lib.rs
@@ -1,9 +1,9 @@
-use std::{sync::Mutex, time::Duration};
+use std::{sync::{Mutex, OnceLock}, time::Duration};
 
 pub use linkme::distributed_slice;
 use linkme::DistributedSlice;
 pub use testme_derive::testme;
-use tokio::{sync::oneshot::Sender, task::JoinError};
+use tokio::{sync::oneshot::{Receiver, Sender}, task::JoinError};
 
 /// reads the command line arg that the test was invoked with
 /// to see if we should add a loop delay. for most cases the answer should be yes.
@@ -47,15 +47,48 @@ pub struct Test {
     pub callback: Sender<Result<(), JoinError>>,
 }
 
+pub fn submit_test(
+    run_all_lock: &'static OnceLock<Mutex<()>>,
+    test_handles: &'static OnceLock<Mutex<Vec<Test>>>,
+    before_each: DistributedSlice<[fn() -> std::pin::Pin<Box<dyn Future<Output=()> + Send>>]>,
+    before_all: DistributedSlice<[fn() -> std::pin::Pin<Box<dyn Future<Output = ()>>>]>,
+    after_each: DistributedSlice<[fn() -> std::pin::Pin<Box<dyn Future<Output=()> + Send>>]>,
+    after_all: DistributedSlice<[fn() -> std::pin::Pin<Box<dyn Future<Output = ()>>>]>,
+    rx: Receiver<Result<(), JoinError>>,
+    t: Test,
+) {
+    let mutex: &'static Mutex<Vec<Test>> = test_handles.get_or_init(|| Mutex::new(vec![]));
+    let mut handles = mutex.lock().expect("failed to get mutex lock");
+    handles.push(t);
+    drop(handles);
+
+    run_all_tests(
+        run_all_lock,
+        mutex,
+        before_each,
+        before_all,
+        after_each,
+        after_all
+    );
+    if let Err(e) = rx.blocking_recv().expect("tests ended before receiving a result") {
+        if let Ok(reason) = e.try_into_panic() {
+            std::panic::resume_unwind(reason);
+        } else {
+            panic!("test case was cancellled?");
+        }
+    }
+}
+
 pub fn run_all_tests(
-    run_all_lock: Mutex<()>,
-    test_handles: Mutex<Vec<Test>>,
+    run_all_lock: &'static OnceLock<Mutex<()>>,
+    test_handles: &'static Mutex<Vec<Test>>,
     before_each: DistributedSlice<[fn() -> std::pin::Pin<Box<dyn Future<Output=()> + Send>>]>,
     before_all: DistributedSlice<[fn() -> std::pin::Pin<Box<dyn Future<Output = ()>>>]>,
     after_each: DistributedSlice<[fn() -> std::pin::Pin<Box<dyn Future<Output=()> + Send>>]>,
     after_all: DistributedSlice<[fn() -> std::pin::Pin<Box<dyn Future<Output = ()>>>]>,
 ) {
-    if let Ok(l) = run_all_lock.try_lock() {
+    let run_all_mutex = run_all_lock.get_or_init(|| Mutex::new(()));
+    if let Ok(l) = run_all_mutex.try_lock() {
         let should_loop = should_loop(after_all);
         // if we got the lock, then we are the test that will drive the others.
         // we will have one runtime, and all the other tests will be ran in this runtime
